e1462a4b38822636b8a16ebc1b63fdc54a09eef5
root <root@menta.me>
2019-04-18 19:30:04 +0900

    mysqldumpを簡易的に修正

--- wpdb_backup.sh
+++ wpdb_backup.sh
@@ -3,12 +3,12 @@
 # mysqlのバックアップ先を指定
 MYS_BKDIR="/home/BKUP/DB"
 TODAYS_MYS_BKDIR="$MYS_BKDIR/`date +%Y%m%d`"
-
+DUMP="$TODAYS_MYS_BKDIR/all_db.dump"
 # 当日分のDBバックアップ格納先を作成
 [ ! -d $TODAYS_MYS_BKDIR ] && mkdir -p $TODAYS_MYS_BKDIR
 echo $TODAYS_MYS_BKDIR 
 # DBのダンプを取得して格納
-mysqldump --defaults-extra-file=my.conf --opt --skip-lock-tables -A | gzip > $TODAYS_MYS_BKDIR/all_db.dump.gz
+mysqldump -u 'root' -p 'f3Dd3<Diz' -A > $DUMP 
 # (毎日実行前提)5日前のバックアップのみ削除します。
 OLD_DATE=`date +%Y%m%d --date "5 days ago"`
 rm -rf $MYS_BKDIR/$OLD_DATE
